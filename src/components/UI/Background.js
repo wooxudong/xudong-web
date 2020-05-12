cf0108a9dba339c15192365862190541d97cf385
Wu Xudong <wooxudong@gmail.com>
2020-05-12 13:25:25 +0800

    fixed the backgound image for gatsby image optimization

--- src/components/UI/Background.js
+++ src/components/UI/Background.js
@@ -1,61 +1,60 @@
 import React from "react";
-import makeStyles from "@material-ui/styles/makeStyles/makeStyles";
 import {graphql, useStaticQuery} from 'gatsby';
+import Img from 'gatsby-image';
+import withStyles from "@material-ui/core/styles/withStyles";
 
-const useStyles = makeStyles({
-  background: {
-    transform: "scale(1)",
-    position: "fixed",
+const styles = {
+  image:{
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    width: "100%",
+    height: "100vh",
+    zIndex: 1,
+  },
+  overlay:{
+    position: 'fixed',
     top: 0,
     left: 0,
     width: "100%",
     height: "100vh",
-    zIndex: 1,
-    "&:before, &:after": {
+    "&:before": {
       content: '""',
       display: "block",
-      position: "absolute",
       top: 0,
       left: 0,
       width: "100%",
-      height: "100%"
-    },
-    "&:before": {
+      height: "100%",
+      zIndex: 2,
+      position: "absolute",
       backgroundImage: `linear-gradient(
         to top,
         rgba(19, 21, 25, 0.5),
         rgba(19, 21, 25, 0.5)
       )`,
-      backgroundSize: "auto, 256px 256px",
-      backgroundPosition: "center, center",
-      backgroundRepeat: "no-repeat, repeat",
-      zIndex: 2
+    backgroundSize: "auto, 256px 256px",
+    backgroundPosition: "center, center",
+    backgroundRepeat: "no-repeat, repeat",
     },
-    "&:after": {
-      backgroundImage: props => `url(${props.src})`,
-      backgroundPosition: "center",
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
-      zIndex: 1
-    }
-  },
-});
+  }
+};
 
-const Background = () => {
+const Background = ({classes}) => {
   const data = useStaticQuery(graphql`
     query BackgroundImageQuery {
       backgroundImage: file(relativePath: { eq: "background.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 1024) {
-            src
+            ...GatsbyImageSharpFluid_tracedSVG
           }
         }
       }
     }`);
 
-  const classes = useStyles(data.backgroundImage.childImageSharp.fluid);
-
-  return <div className={`${classes.background}`} />;
+  return(
+    <div className={classes.overlay}>
+      <Img fluid={data.backgroundImage.childImageSharp.fluid} className={classes.image}/>
+    </div>);
 };
 
-export default Background;
+export default withStyles(styles)(Background);
