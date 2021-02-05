116dcad0bcb78f6a88669e64123bca9a59e7a7f9
Wu Xudong <wooxudong@gmail.com>
2021-02-05 12:22:23 +0800

    added more quote types and code types

--- src/components/styles/colors.js
+++ src/components/styles/colors.js
@@ -4,6 +4,10 @@ export const black = 'black';
 export const darkGreen = '#698474';
 export const grey = '#3b3a37';
 export const lightGrey = '#f3f5f2';
+export const red = '#ec4646';
+export const lightRed = '#ffdcdc';
+export const yellow = '#f58634';
+export const lightYellow = '#f7f6e7';
 
 export default {
   mainGreen,
@@ -12,4 +16,8 @@ export default {
   darkGreen,
   grey,
   lightGrey,
+  red,
+  lightRed,
+  yellow,
+  lightYellow,
 };
